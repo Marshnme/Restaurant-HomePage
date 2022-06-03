ab3a3f949967c8e30edc34653571f90ba655f05e
Marshnme <jildsmiles@yahoo.com>
2022-06-03 16:20:37 -0400

    setting up svg for menu page use

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,11 +1,11 @@
-
+import icon from "../images/ramen.svg";
 
 
 const menu = () => {
     const mainEle = document.querySelector("main");
 
     let infoContainer = document.createElement("div");
-    infoContainer.classList.add("info-contain")
+    infoContainer.classList.add("info-contain", "info-contain-menu")
     
     let menuTitle = document.createElement("h1");
     menuTitle.classList.add("name")
@@ -14,7 +14,12 @@ const menu = () => {
     mainEle.appendChild(menuTitle)
     mainEle.appendChild(infoContainer)
 
+    let menuItemOneContain = document.createElement("div")
+    let imgTag = document.createElement("img")
+    imgTag.src = icon
+    imgTag.width = "80"
     
+    infoContainer.appendChild(imgTag)
     
 }
 
